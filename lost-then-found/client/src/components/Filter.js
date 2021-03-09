1910c5a99ae6eebc116a8a30e0d211dc910f3cb7
Emily Hodges <ehodges9@gatech.edu>
2021-03-08 20:53:02 -0500

    Create Filter component, start on input tagss

---
+++ lost-then-found/client/src/components/Filter.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Popup from "./TagsInput";
+
+const Filter = () => {
+
+    return (
+        <div class="dropdown">
+            <h1>Filter By</h1>
+            <label for="itemDate">Date: </label>
+                <input type="date" id="itemDate" name="itemDate" className="postInput2"></input> 
+                <br></br>
+            <label for="time" classsName="inputLabel">Time: </label>
+                <input type="time" id="time" name="time" className="postInput2"></input>
+                <br></br>
+
+            <TagsInput></TagsInput>
+                    
+            <br></br>
+
+            <div id="locationList">
+
+            </div>
+                
+        </div>
+
+    );
+}
+
+export default Filter;
