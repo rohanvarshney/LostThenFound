68a4714fe0d83e47c93d3128fa9350167da16413
Ski <ski.ingurgio@gmail.com>
2021-03-15 22:17:32 -0400

    Validation files
    Should allow for validation of registration and log in

--- lost-then-found/server/Model/User.js
+++ lost-then-found/server/Model/User.js
@@ -30,10 +30,12 @@ const UserSchema = new Schema({
         type: String
     },
     email: {
-        type: String
+        type: String,
+        required: true
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     date_created: {
         type: Date,
