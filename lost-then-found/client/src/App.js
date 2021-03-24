ca5198627b0abbb1025864b2ce7819ccbdeaa460
Ski <ski.ingurgio@gmail.com>
2021-03-24 06:06:28 -0400

    Matches is now a private route
    coincidentally fixed crash issue, now matches page will redirect to login if not already logged in

--- lost-then-found/client/src/App.js
+++ lost-then-found/client/src/App.js
@@ -7,6 +7,9 @@ import {
   Link,
   useParams
 } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store";
 import './App.css';
@@ -18,6 +21,28 @@ import Nav from "./components/Nav";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import {NavLink} from "react-router-dom";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 
 function App() {
 
@@ -65,7 +90,7 @@ function App() {
                   setData={setItemData}/> }
                   exact
                   />
-                  <Route path="/Matches" component={Matches}/>
+                  <PrivateRoute exact path="/Matches" component={Matches}/>
                   <Route path="/" component={Home}/>
                 </Switch>
             </div>
