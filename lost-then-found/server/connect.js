933b7e65fd56da051ba6f3fc194bd722d100ff86
dbehailu <dagmbehailu@gmail.com>
2021-04-09 14:40:33 -0400

    prod specification and package

--- lost-then-found/server/connect.js
+++ lost-then-found/server/connect.js
@@ -6,6 +6,7 @@ const emails = require('./routes/api/emails');
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 
@@ -158,6 +159,16 @@ app.use("/api/users", users);
 app.use('/api/posts', posts);
 
 app.use('/api/emails', emails);
+// serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+
+  app.use(express.static('../client/build'));
+  
+  app.get("*", (req,res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
