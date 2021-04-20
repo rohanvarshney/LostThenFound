26904f11da037e195661814162fc63d3f0ba0c38
Ski <ski.ingurgio@gmail.com>
2021-04-19 21:17:15 -0400

    things to make the demo easier

--- lost-then-found/client/src/components/Popup.js
+++ lost-then-found/client/src/components/Popup.js
@@ -86,6 +86,7 @@ const Popup = (props) => {
                             <input onChange={event => setLocation(event.target.value)} list="locations" name="loc" id="loc" className="postInput2" id="locInput"></input>
                             <datalist id="locations">
                                 <option value="Clough Undergraduate Learning Commons"></option>
+                                <option value="Tech Green"></option>
                                 <option value="Klaus College of Computing"></option>
                                 <option value="Student Center"></option>
                                 <option value="Howey Physics Building"></option>
