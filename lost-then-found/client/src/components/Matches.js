fda1b1546766ea9c61935d464c4d835cf39a28ed
Rohan Varshney <rohanvar@gmail.com>
2021-04-21 15:36:56 -0400

    Matching Functionality Fixed
    actually! HOOAH

--- lost-then-found/client/src/components/Matches.js
+++ lost-then-found/client/src/components/Matches.js
@@ -48,6 +48,30 @@ const Matches = (props) => {
   console.log("Total Items: " + posts.length);
 
 
+
+
+  function hasWordMatch(a, b, case_sensitive) {
+    if (case_sensitive !== true) {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+    }
+    var a_parts = a.split(' ');
+    var b_parts = b.split(' ');
+    var a_length = a_parts.length;
+    var b_length = b_parts.length;
+    var i_a, i_b;
+
+    for (i_a = 0; i_a < a_length; i_a += 1) {
+        for (i_b = 0; i_b < b_length; i_b += 1) {
+            if (a_parts[i_a] === b_parts[i_b]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
   function isMatch(myPost, otherPost) {
     // TODO: CHECK FOR NULL VALUE COMPARISONS, EMPTY LISTS, ETC.
     // console.log(myPost);
@@ -60,57 +84,71 @@ const Matches = (props) => {
     */
 
     // same post isn't match
-    if (myPost === otherPost) return false;
+    if (myPost === otherPost) {
+      console.log("Same post!");
+      return false;
+    }
     // either post is null or empty or undefined
-    if (!myPost || !otherPost) return false;
+    if (!myPost || !otherPost) {
+      console.log("Post is empty or undefined!");
+      return false;
+    }
 
     // found item to lost item or vice versa is needed
-    if (myPost.found == otherPost.found) return false;
+    if (myPost.found == otherPost.found) {
+      console.log("Not opposite statuses!");
+      return false;
+    }
 
     // 1
     const otherPostTags = otherPost.tags;
     for (var tag in myPost.tags) {
-      if (otherPostTags.includes(tag)) {
-        return true;
+      if (tag === "" || tag === '0') continue;
+      for (var tag2 in otherPostTags) {
+        if (tag2 === "" || tag2 === '0') continue;
+        if (tag === tag2) {
+          console.log(tag + " === " + tag2 + "?");
+          return true;
+        }
       }
     }
 
     //2
     // TODO: Make this better
-    var otherWords = otherPost.post_title.split(" ");
-    var myWords = myPost.post_title.split(" ");
-    for (var word in otherWords) {
-      if (myWords.includes(word)) {
-        return true;
-      }
+    if (hasWordMatch(otherPost.post_title, myPost.post_title)) {
+      console.log("Similar titles!");
+      return true;
     }
 
     return false;
   }
 
 
-  function matchesOfSpecifiedPost(post_title) {
+  function matchesOfSpecifiedPost(postTitle, userPosts) {
     var matchedPosts = [];
     var user = props.auth.user;
     console.log(user);
     var specifiedPost = null;
 
-    for (let a = 0; a < originalPosts.length - 1; a++) {
-      var postInQuestion = originalPosts[a];
-      if (postInQuestion.post_title === post_title) {
+    for (let a = 0; a < userPosts.length; a++) {
+      var postInQuestion = userPosts[a];
+      console.log("Title: " + postInQuestion.post_title);
+      if (postInQuestion.post_title === postTitle) {
         specifiedPost = postInQuestion;
         break;
       }
     }
 
+    console.log(specifiedPost);
     if (specifiedPost == null) {
       return matchedPosts;
     }
 
 
-    for (let a = 0; a < originalPosts.length - 1; a++) {
+    for (let a = 0; a < originalPosts.length; a++) {
       var possibleMatchPost = originalPosts[a];
       var match = isMatch(specifiedPost, possibleMatchPost);
+      console.log(specifiedPost.post_title + ", " + possibleMatchPost.post_title + " = " + match);
       if (match) {
         matchedPosts.push(possibleMatchPost);
       }
@@ -162,7 +200,8 @@ const Matches = (props) => {
     } else {
       const postItemTitle = e.target.value;
       console.log(postItemTitle);
-      var matchedPosts = matchesOfSpecifiedPost(postItemTitle)
+      var user_posts = getMyPosts(originalPosts);
+      var matchedPosts = matchesOfSpecifiedPost(postItemTitle, user_posts)
       updatePosts(matchedPosts);
       console.log(posts);
     }
