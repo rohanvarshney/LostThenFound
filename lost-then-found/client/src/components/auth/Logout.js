48de046302a0bf234cb558fb5660200c93979a56
Ski <ski.ingurgio@gmail.com>
2021-04-22 19:29:24 -0400

    Minor fix

--- lost-then-found/client/src/components/auth/Logout.js
+++ lost-then-found/client/src/components/auth/Logout.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 
 class Logout extends Component {
@@ -17,13 +17,13 @@ class Logout extends Component {
   }
 
 	render() {
-	    const { user } = this.props.auth;
+		const auth = this.props.auth;
 	  return (
 	      <div style={{ height: "75vh" }} className="container valign-wrapper">
 	        <div className="row">
 	          <div className="col s12 center-align">
 	            <h4>
-	              <b>Hey there, User!</b>
+	              <b>Hey there, </b>{auth.user.name}!
 	              <p className="flow-text grey-text text-darken-1">
 	                You are now logged in!
 	              </p>
